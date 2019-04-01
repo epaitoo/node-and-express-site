d1d70fb63f0cb45c852e77e587dbd9e9fd359295
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-04-01 19:43:59 +0000

    add additional files

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+app.get('/project', (req, res) => {
+    res.render("project");
+});
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost 3000');
+})
